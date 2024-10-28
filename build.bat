a72cca670a0b7a811ee391f1bfb19d51a837f395
Furkan Sarihan <fusarihan@gmail.com>
2024-10-28 22:56:33 +0300

    creating a window

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@
 setlocal
 
 set BUILD_DIR="build"
+set BUILD_TYPE="Debug"
 
 if %CLEAN_BUILD%==true (
   rmdir /s /q %BUILD_DIR%
@@ -12,7 +13,9 @@ if not exist %BUILD_DIR% mkdir %BUILD_DIR%
 
 pushd %BUILD_DIR%
 
-cmake .. -G "MinGW Makefiles"
+conan install .. --output-folder=. --build=missing --settings=build_type=%BUILD_TYPE%
+
+cmake .. -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
 cmake --build .
 
 virtuniverse.exe
