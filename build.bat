c374300a63986dd740cbbfd59a9035de0d759bed
AJA <Maneerat3d@gmail.com>
2025-10-08 00:59:23 +0700

    fix windows vs 17 build

--- build.bat
+++ build.bat
@@ -4,7 +4,8 @@ setlocal
 set BUILD_DIR="build"
 set BUILD_TYPE="Debug"
 
-if %CLEAN_BUILD%==true (
+REM แก้ไขโดยการเพิ่ม "" ครอบตัวแปรและค่าที่เปรียบเทียบ
+if "%CLEAN_BUILD%"=="true" (
   rmdir /s /q %BUILD_DIR%
   mkdir %BUILD_DIR%
 )
@@ -13,9 +14,10 @@ if not exist %BUILD_DIR% mkdir %BUILD_DIR%
 
 pushd %BUILD_DIR%
 
+REM รวมคำสั่ง conan install ให้อยู่ในบรรทัดเดียวกัน
 conan install .. --output-folder=. --build=missing --settings=build_type=%BUILD_TYPE%
 
-cmake .. -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
+cmake .. -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
 cmake --build .
 
 virtuniverse.exe
